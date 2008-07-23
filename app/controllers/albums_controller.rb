8e19cbee800e4f09eea4aeea79cca69aa884cfb4
Dave Rapin <dave@rapin.com>
2008-07-23 17:54:24 -0400

    styles

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,49 +1,7 @@
-class AlbumsController < ApplicationController
-  
-  def index
-    @albums = Album.find :all  
-  end
-  
-  def new
-    @album = Album.new
-  end
+class AlbumsController < ResourceController::Base
   
   def show
     @album = Album.find(params[:id])
-    @photo = Photo.new
-  end
-  
-  def edit
-    @album = Album.find(params[:id])  
-  end
-  
-  def create
-    @album = Album.new(params[:album])
-    
-    if @album.save
-       flash[:notice] = 'Album was successfully created.'
-       redirect_to albums_url 
-    else
-       render :action => "new" 
-    end
-  end
-  
-  def update
-    @album = Album.find(params[:id])
-
-    if @album.update_attributes(params[:album])
-      flash[:notice] = 'Album was successfully updated.'
-      redirect_to albums_url
-    else
-      render :action => "edit"
-    end
-  end
-  
-  def destroy
-    @album = Album.find(params[:id])
-    @album.destroy
-    
-    redirect_to albums_url
   end
   
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,33 +1,5 @@
-class PhotosController < ApplicationController
+class PhotosController < ResourceController::Base
 
-  def create
-    _find_album
-    @photo = Photo.new(params[:photo])
-    
-    if @photo.save
-      @album.photos << @photo
-      flash[:notice] = "Album was successfully created."
-      redirect_to albums_url
-    else
-      flash[:notice] = "File size cannot exceed 500 kb"
-      redirect_to album_url(@album)
-    end
-       
-  end
-  
-  def destroy
-    _find_album
-    @photo = @album.photos.find(params[:id])
-    @photo.destroy
-    flash[:notice] = "Photo was successfully deleted."
-    redirect_to albums_url
-  end
-  
-  
-  private #-------
-  
-  def _find_album
-    @album = Album.find(params[:album_id])
-  end
+  belongs_to :album
   
 end
