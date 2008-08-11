7c66b56512fcd141cdf4fed2d9c0ca8cd0da08be
Dave Rapin <dave@rapin.com>
2008-08-11 12:07:05 -0400

    removed permalink

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,6 +6,6 @@ class Album < ActiveRecord::Base
   has_many :photos, :dependent => :destroy, :order => 'position'
   has_many :visible_photos, :class_name => 'Photo', :conditions => 'visible = 1', :order => 'position'
   
-  has_permalink :title
+  has_friendly_id :title
   
 end
