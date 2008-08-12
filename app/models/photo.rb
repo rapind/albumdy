74156363429174a4aa73eb04c0cb188fad23c6e4
Dave Rapin <dave@rapin.com>
2008-08-12 16:03:18 -0400

    changed file storage to S3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,12 @@ class Photo < ActiveRecord::Base
   belongs_to :album, :counter_cache => true
   acts_as_list :scope => :album
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :album_cover => '180x180>', :thumb => "100x100>" }
+  has_attached_file :image, 
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+                    :path => ":attachment/:id/:style.:extension",
+                    :bucket => 'albumdy',
+                    :styles => { :medium => "300x300>", :album_cover => '180x180>', :thumb => "100x100>" }
   
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => 'image/jpeg'
