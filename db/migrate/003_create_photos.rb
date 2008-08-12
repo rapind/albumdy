d2949212405d2e64ccbe33d2805c2b43abe1d07f
Dave Rapin <dave@rapin.com>
2008-08-12 15:47:16 -0400

    fixed ignore

--- db/migrate/003_create_photos.rb
+++ db/migrate/003_create_photos.rb
@@ -13,6 +13,7 @@ class CreatePhotos < ActiveRecord::Migration
       t.string :image_file_name # Original filename
       t.string :image_content_type # Mime type
       t.integer :image_file_size # File size in bytes
+      t.datetime :image_updated_at
       
       t.timestamps
     end
