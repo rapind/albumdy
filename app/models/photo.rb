54742acbadeae38783777183fc23ae8587af6a8a
Dave Rapin <dave@rapin.com>
2008-08-12 15:33:06 -0400

    compressed jquery ui components

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,9 @@ class Photo < ActiveRecord::Base
   
   has_attached_file :image, :styles => { :medium => "300x300>", :album_cover => '180x180>', :thumb => "100x100>" }
   
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, :content_type => 'image/jpeg'
+  
   # Fix the mime types. Make sure to require the mime-types gem
   def swfupload_file=(data)
     data.content_type = MIME::Types.type_for(data.original_filename).to_s
