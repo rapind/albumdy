3b69f72e3fc22dfe0efefdcd6a16e2e422cff80b
Dave Rapin <dave@rapin.com>
2008-08-14 09:05:05 -0400

    switched back to s3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,14 +4,14 @@ class Photo < ActiveRecord::Base
   acts_as_list :scope => :album
   
   has_attached_file :image, 
-                    :path => ":rails_root/public/photos/:id/:style_:basename.:extension",
-                    :url => "/photos/:id/:style_:basename.:extension",
+                    #:path => ":rails_root/public/photos/:id/:style_:basename.:extension",
+                    #:url => "/photos/:id/:style_:basename.:extension",
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
+                    :path => "photos/:id/:style_:basename.:extension",
+                    :bucket => 'albumdy',
                     :styles => { :medium => "300x300>", :album_cover => '180x180>', :thumb => "100x100>" }
-  
-                    #:storage => :s3,
-                    #:s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml",
-                    #:path => "photos/:id/:style.:extension",
-                    #:bucket => 'albumdy',
+                    
                     
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => 'image/jpeg'
