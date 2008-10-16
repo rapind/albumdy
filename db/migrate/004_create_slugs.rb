8d7376f37baf212c4b597dfd8747e964b4b69330
Dave Rapin <dave@rapin.com>
2008-10-16 15:46:03 -0400

    fixed slugs migration for production

--- db/migrate/004_create_slugs.rb
+++ db/migrate/004_create_slugs.rb
@@ -1,12 +1,15 @@
 class CreateSlugs < ActiveRecord::Migration
   def self.up
     create_table :slugs do |t|
-      t.string :name
-      t.string :sluggable_type
-      t.integer :sluggable_id
+      t.string :name, :limit => 255, :null => false
+      t.string :sluggable_type, :limit => 128, :null => false
+      t.integer :sluggable_id, :null => false
       t.timestamps
     end
-    add_index :slugs, [:name, :sluggable_type], :unique
+    
+    #add_index :slugs, [:name, :sluggable_type], :unique => true
+    add_index :slugs, :name
+    add_index :slugs, :sluggable_type
     add_index :slugs, :sluggable_id
   end
 
